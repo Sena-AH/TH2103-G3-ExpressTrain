0eedac697ddddd7ddba329b8912cbd10b86839dd
Joakim Widell <70150296+joakimwidell@users.noreply.github.com>
2021-12-16 12:27:04 +0100

    About page design

--- client/src/components/AboutPage.jsx
+++ client/src/components/AboutPage.jsx
@@ -1,8 +1,18 @@
+import '../css/about.css';
 
 function AboutPage() {
     return (
       <div>
-        <p>hello AboutPage</p>
+        <div className="about-div">
+          <div className="grey-rectangle-div"></div>
+          <div className="about-font">ABOUT</div>
+          <div className="upper-content-font">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non eros sagittis, auctor neque sit amet, blandit lorem. Etiam molestie.  
+          </div>
+          <br></br>
+          <div className="lower-content-div">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non eros sagittis, auctor neque sit amet, blandit lorem. Etiam molestie semper mi, ac lacinia justo vulputate quis. Morbi id auctor nibh. Morbi vestibulum bibendum urna id sollicitudin. Suspendisse eu molestie nulla. Maecenas ut convallis ante, eu pretium orci. Cras sed ex massa. Eeey macarena Curabitur odio massa, ultricies at euismod ut, bibendum non tellus.          </div>
+          </div>
       </div>
     );
   };
