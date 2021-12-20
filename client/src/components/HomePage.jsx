4f49a85a60512bb3294badf2d7663cafbdaf0678
KennySohl <kenny.sohl@gmail.com>
2021-12-20 13:12:12 +0100

    changed html/css for homepage
    changed html/css for homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -11,46 +11,57 @@ function HomePage() {
 
   return (
     <main>
-      <div className="input-search">
-        <input value="To"></input>
-      </div>
-      <div className="input-search">
-        <input value="From"></input>
-      </div>
-
-      <form className="input-form" action="">
-        <div>
-          <input type="radio" id="oneWay" name="travelWay" value="oneWay" />
-          <label htmlFor="oneWay">One Way</label>
+      <div className="wrapper">
+        <div className="input-search">
+          <input className="input" placeholder="Till:"></input>
         </div>
-        <div>
-          <input type="radio" id="roundTrip" name="travelWay" value="roundTrip" />
-          <label htmlFor="roundTrip">Round Trip</label>
-
+        <div className="input-search">
+          <input className="input" placeholder="Från:"></input>
         </div>
-      </ form>
 
-      <div className="input-tickets">
-        <select name="tickets" id="tickets">
-          <option value="1">1 ticket</option>
-          <option value="2">2 tickets</option>
-          <option value="3">3 tickets</option>
-          <option value="4">4 tickets</option>
-          <option value="5">5 tickets</option>
-          <option value="6">6 tickets</option>
-          <option value="7">7 tickets</option>
-          <option value="8">8 tickets</option>
-        </select>
+        <form className="input-form" action="">
+          <div className="switch">
+                    <input type="radio"
+                           className="switch-input"
+                           name="view"
+                           value="oneway"
+                           id="oneway"
+                           checked
+                    />
+                    <label htmlFor="oneway" className="switch-label switch-label-off">Enkelresa</label>
+
+                    <input type="radio"
+                           className="switch-input"
+                           name="view"
+                           value="roundtrip"
+                           id="roundtrip"
+                    />
+                    <label htmlFor="roundtrip" className="switch-label switch-label-on">Tur & retur</label>
+                    <span className="switch-selection"></span>
+          </div>
+        </ form>
+
+        <div className="input-tickets">
+          <select className="input-color" name="tickets" id="tickets">
+            <option value="1">1 Biljett</option>
+            <option value="2">2 Biljett</option>
+            <option value="3">3 Biljett</option>
+            <option value="4">4 Biljett</option>
+            <option value="5">5 Biljett</option>
+            <option value="6">6 Biljett</option>
+            <option value="7">7 Biljett</option>
+            <option value="8">8 Biljett</option>
+          </select>
+        </div>
+
+        <div className="input-date">
+          <input className="input-color" type="date"></input>
+        </div>
+
+        <div className="search-btn">
+          <button type="button" onClick={handleClick}>Sök</button>
+        </div>
       </div>
-
-      <div className="input-date">
-        <input type="date"></input>
-      </div>
-
-      <div className="search-btn">
-        <button type="button" onClick={handleClick}>Search</button>
-      </div>
-
     </main>
   );
 };
