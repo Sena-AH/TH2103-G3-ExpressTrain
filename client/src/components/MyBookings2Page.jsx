85b894090411b2f734d8dd00faf7690c71ab6938
Alejandratala <70092696+Alejandratala@users.noreply.github.com>
2021-12-23 11:34:34 +0100

    final changes

--- client/src/components/MyBookings2Page.jsx
+++ client/src/components/MyBookings2Page.jsx
@@ -1,286 +1,237 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {useLocation, useNavigate} from 'react-router-dom';
+import '../css/myBookings2Page.css'; 
 
 function MyBookings2Page() {
   const navigate = useNavigate();
-  const { state } = useLocation();
+  const {state} = useLocation();
   const bookingId = state.bookingId;
 
-  const [booking, setBooking] = useState("");
-  const [traveller, setTraveller] = useState("");
-  const [stages, setStages] = useState("");
+  const [booking, setBooking] = useState([]);
+  const [traveller, setTraveller] = useState([]);
+  const [stages, setStages] = useState([]);
   const [schedules, setSchedules] = useState([]);
-  const [trainStations, setTrainStations] = useState([]);
+  const [stations, setStations] = useState([]);
+  const [platforms, setPlatforms] = useState([]);
   const [error, setError] = useState(null);
 
-  var bookingCache = "";
-  var travellerCache = "";
-  var stagesCache = "";
-  var schedulesCache = [];
-  var trainStationsCache = [];
+  useEffect(() => {
+    (async () => {
+      setBooking(await fetchBooking());
+    })();
+  }, [bookingId]);
 
   useEffect(() => {
-    async function fetchBooking(id) {
-      return await fetch(`/api/booking/${id}`)
-        .then(response => response.json());
-    }
-
-    async function fetchTraveller(id) {
-      return await fetch(`/api/Traveller/${id}`)
-      .then(response => response.json());
-    }
-
-    async function fetchScheduleStages(bookingId) {
-      return await fetch(`/api/schedulestage/booking/${bookingId}`)
-      .then(response => response.json());
-    }
-
-    async function fetchSchedules(stages) {
-      for (let i = 0; i < stages.length; i++) {
-        const stage = stages[i];
-        let result = await fetch(`/api/Schedule/${stage.ScheduleId}`)
-          .then(response => response.json());
-        setSchedules(schedules => [...schedules, result])
-      }
-    }
-    
-    async function fetchTrainStations(schedule) {
-      // return await fetch(`/api/TrainStation/{id}`)
-      // .then(response => response.json())
-    }
-
+    if (!isObjLoaded(booking)) return;
     (async () => {
-      bookingCache = await fetchBooking(bookingId);
-      setBooking(bookingCache);
-
-      travellerCache = await fetchTraveller(bookingCache.TravellerId);
-      setTraveller(travellerCache);
-
-      stagesCache = await fetchScheduleStages(bookingId);
-      setStages(stagesCache);
-
-      await fetchSchedules(stagesCache);
-      
-      trainStationsCache = await fetchTrainStations();
+      setTraveller(await fetchTraveller(booking.TravellerId));
     })();
+  }, [booking]);
 
-    // if ((stages !== "") && (schedules.length === 0)) {
-    //   stages.forEach(stage => {
-    //     fetchSchedule(stage.ScheduleId);
-    //   });
-    // }
-    // if ((schedules.length > 0) && (Object.keys(trainStations).length === 0)) {
-    //   schedules.forEach(schedule => {
-    //     fetchTrainStation(schedule.DepartureTrainStationId);
-    //     fetchTrainStation(schedule.DestinationTrainStationId);
-    //   });
-    // }
+  useEffect(() => {
+    if (!isObjLoaded(traveller)) return;
+    (async () => {
+      setStages(await fetchStages());
+    })();
+  }, [traveller]);
 
-    // async function fetchBookingOld() {
-    //   await fetch(`/api/booking/${bookingId}`)
-    //     .then(response => {
-    //       if (!response.ok) {
-    //         setError(response.status);
-    //       }
-    //       return response.json();
-    //     })
-    //     .then(
-    //       result => {
-    //         setBooking(result);
-    //       },
-    //       error => {
-    //         setError(error);
-    //       }
-    //     );
-    // }
+  useEffect(() => {
+    if (!isObjLoaded(stages)) return;
+    (async () => {
+      setSchedules(await fetchSchedules(stages));
+    })();
+  }, [stages]);
 
-    // async function fetchTravellerOld() {
-    //   await fetch(`/api/Traveller/${booking.TravellerId}`)
-    //     .then(response => {
-    //       if (!response.ok) {
-    //         setError(response.status);
-    //       }
-    //       return response.json();
-    //     })
-    //     .then(
-    //       result => {
-    //         setTraveller(result);
-    //       },
-    //       error => {
-    //         setError(error);
-    //       }
-    //     );
-    // }
+  useEffect(() => {
+    if (!isObjLoaded(schedules)) return;
+    (async () => {
+      setStations(await fetchStations(schedules));
+      setPlatforms(await fetchPlatforms(schedules));
+    })();
+  }, [schedules]);
 
-    // async function fetchScheduleStagesOld() {
-    //   await fetch(`/api/schedulestage/booking/${bookingId}`)
-    //     .then(response => {
-    //       if (!response.ok) {
-    //         setError(response.status);
-    //       }
-    //       return response.json();
-    //     })
-    //     .then(
-    //       result => {
-    //         setStages(result);
-    //       },
-    //       error => {
-    //         setError(error);
-    //       }
-    //     );
-    // }
-
-    // async function fetchScheduleOld(id) {
-    //   await fetch('/api/Schedule/' + id)
-    //     .then(response => {
-    //       if (!response.ok) {
-    //         setError(response.status);
-    //       }
-    //       return response.json();
-    //     })
-    //     .then(
-    //       result => {
-    //         setSchedules(schedules => [...schedules, result])
-    //       },
-    //       error => {
-    //         setError(error);
-    //       }
-    //     );
-    // }
-
-    // async function fetchTrainStation(id) {
-    //   await fetch('/api/TrainStation/' + id)
-    //     .then(response => {
-    //       if (!response.ok) {
-    //         setError(response.status);
-    //       }
-    //       return response.json();
-    //     })
-    //     .then(
-    //       result => {
-    //         // setTrainStations(trainStations => ({...trainStations, result}))
-    //         if (!(result.Id in trainStations)) {
-    //           setTrainStations(trainStations => ({
-    //             ...trainStations,
-    //             [result.Id]: result
-    //           }));
-    //         }
-    //       },
-    //       error => {
-    //         setError(error);
-    //       }
-    //     );
-    // }
-
-    // if (booking === "") {
-    //   fetchBooking();
-    // }
-    // if ((booking !== "") && (traveller === "")) {
-    //   fetchTraveller();
-    // }
-    // if ((traveller !== "") && (stages === "")) {
-    //   fetchScheduleStages();
-    // }
-    // if ((stages !== "") && (schedules.length === 0)) {
-    //   stages.forEach(stage => {
-    //     fetchSchedule(stage.ScheduleId);
-    //   });
-    // }
-    // if ((schedules.length > 0) && (Object.keys(trainStations).length === 0)) {
-    //   schedules.forEach(schedule => {
-    //     fetchTrainStation(schedule.DepartureTrainStationId);
-    //     fetchTrainStation(schedule.DestinationTrainStationId);
-    //   });
-    // }
-
-  }, [bookingId]); //[bookingId, booking, traveller, stages, schedules, trainStations]);
+  function isObjLoaded(state) {
+    return !(Object.keys(state).length === 0);
+  }
 
   function handleClick() {
     navigate("/MyBookings3Page");
   }
-  
+
+  async function fetchBooking() {
+    return await fetchUrl(`/api/booking/${bookingId}`, 'booking');
+  }
+
+  async function fetchTraveller(id) {
+    return await fetchUrl(`/api/Traveller/${id}`, 'traveller');
+  }
+
+  async function fetchStages() {
+    return await fetchUrl(`/api/ScheduleStage/booking/${bookingId}`, 'stages');
+  }
+
+  async function fetchSchedules(stages) {
+    let schedules = [];
+    for (const stage of stages) {
+      const schedule = await fetchUrl(`/api/Schedule/${stage.ScheduleId}`, 'schedules');
+      schedule.DepartureTime = new Date(schedule.DepartureTime);
+      schedule.ArrivalTime = new Date(schedule.ArrivalTime);
+      schedules.push(schedule);
+    }
+    return schedules;
+  }
+
+  async function fetchStations(schedules) {
+    let stations = {};
+    for (const schedule of schedules) {
+      const departureStationId = schedule.DepartureTrainStationId;
+      if (!(departureStationId in stations)) {
+        const departureStation = await fetchUrl(`/api/TrainStation/${departureStationId}`, 'stations');
+        stations[departureStation.Id] = departureStation;
+      }
+      const destinationStationId = schedule.DestinationTrainStationId;
+      if (!(destinationStationId in stations)) {
+        const destinationStation = await fetchUrl(`/api/TrainStation/${destinationStationId}`, 'stations');
+        stations[destinationStation.Id] = destinationStation;
+      }
+    }
+    return stations;
+  }
+
+  async function fetchPlatforms(schedules) {
+    let platforms = {};
+    for (const schedule of schedules) {
+      const platformId = schedule.DeparturePlatformId;
+      if (!(platformId in platforms)) {
+        const platform = await fetchUrl(`/api/TrainStationPlatform/${platformId}`, 'platforms');
+        platforms[platform.Id] = platform;
+      }
+    }
+    return platforms;
+  }
+
+  async function fetchUrl(url, errorMessage = 'unknown') {
+    return await fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          setError(`${response.status} (${errorMessage})`);
+        }
+        return response.json();
+      })
+      .then(result => {
+        return result;
+      }, error => {
+        // setError(`${error} (${errorMessage})`);
+      });
+  }
+
   function Itinerary() {
     let itineraries = [];
-    if (schedules.length > 0) {
-      for (let index = 0; index < schedules.length; index++) {
-        const schedule = schedules[index];
-        const seatNumber = stages[index].SeatNumber;
-        itineraries.push(<div key={index} className="itenerary-result">{schedule.DepartureTrainStationId} - {schedule.DestinationTrainStationId} ({schedule.DepartureTime} - {schedule.ArrivalTime}) - Platform: {schedule.DeparturePlatformId} - Seat: {seatNumber}</div>);
-      }
+    for (let i = 0; i < schedules.length; i++) {
+      const schedule = schedules[i];
+      let departureStation = stations[schedule.DepartureTrainStationId].Name ?? 'unknown';
+      let destinationStation = stations[schedule.DestinationTrainStationId].Name ?? 'unknown';
+      let departurePlatform = platforms[schedule.DeparturePlatformId].Name ?? 'unknown';
+      let departureDate = formatDate(schedule.DepartureTime);
+      let departureTime = formatTime(schedule.DepartureTime);
+      let arrivalTime = formatTime(schedule.ArrivalTime);
+      let seat = stages[i].SeatNumber ?? 'unknown';
+
+      itineraries.push(<div key={schedule.Id} className="itinerary-result">
+        {departureDate}<br/>
+        {departureTime} - {departureStation} (Platform {departurePlatform} - Seat {seat})<br/>
+        {arrivalTime} - {destinationStation} (Platform {departurePlatform} - Seat {seat})
+      </div>);
     }
     return itineraries;
   }
 
+  function itineraryIsLoaded() {
+    return (isObjLoaded(stations) && isObjLoaded(platforms));
+  }
+
+  function formatDate(date) {
+    const [day, month, year] = [to2Digits(date.getDate()), to2Digits(date.getMonth()), date.getFullYear()];
+    return `${year}-${month}-${day}`;
+  }
+
+  function formatTime(date) {
+    const [hour, minutes] = [to2Digits(date.getHours()), to2Digits(date.getMinutes())];
+    return `${hour}:${minutes}`;
+  }
+
+  function to2Digits(value) {
+    return ("0" + value).slice(-2);
+  }
+
   function Booking() {
-    return (
-      <>
-        <div>
-          <h1>Min bokning</h1>
+    // if (!isObjLoaded(platforms)) {
+    //   return(<div>Laddar bokning...</div>);
+    // }
+    const travelDate = isObjLoaded(schedules) ? formatDate((schedules[0].DepartureTime)) : 'loading...';
+    return (<>
+      <div>
+        <h1>Min bokning</h1>
+      </div>
+      <div>
+        <h3>Boking ID: {bookingId}</h3>
+
+        <div className="travel-date">
+          <br/>
+          <div className="travel-date-title">Resdatum:</div>
+          <div className="travel-date-result">{travelDate}</div>
         </div>
 
-        <div>
-          <h3>Boking ID: {bookingId}</h3>
-
-          <div className="travel-date">
-            <br></br>
-            <div className="travel-date-title">Resdag:</div>
-            <div className="travel-date-result">xxx</div>
-          </div>
-          
-          <div className="itenerary">
-            <br></br>
-            <div className="itenerary-title">Resväg:</div>
-            <Itinerary />
-          </div>
-
-          <div className="name">
-            <br></br>
-            <div className="name-title">Name:</div>
-            <div className="name-result">{traveller.FirstName} {traveller.LastName}</div>
-          </div>
-          
-          <div className="email">
-            <br></br>
-            <div className="email-title">Email:</div>
-            <div className="email-result">{traveller.Email}</div>
-          </div>
-
-          <div className="phoneNumber">
-            <br></br>
-            <div className="phoneNumber-title">Phone number:</div>
-            <div className="phoneNumber-result">{traveller.PhoneNumber}</div>
-          </div>
-
-          <div className="price">
-            <br></br>
-            <div className="price-title">Total price:</div>
-            <div className="price-result">{booking.Price} kr</div>
-          </div>
+        <div className="itinerary">
+          <br/>
+          <div className="itinerary-title">Resväg:</div>
+          {itineraryIsLoaded() ? <Itinerary/> : 'loading...'}
         </div>
-        
-        <div className="search-btn">
-          <button type="button" onClick={handleClick}>Cancel Booking</button>
+
+        <div className="name">
+          <br/>
+          <div className="name-title">Namn:</div>
+          <div className="name-result">{traveller.FirstName} {traveller.LastName}</div>
         </div>
-      </>
-    );
+
+        <div className="email">
+          <br/>
+          <div className="email-title">E-post:</div>
+          <div className="email-result">{traveller.Email}</div>
+        </div>
+
+        <div className="phoneNumber">
+          <br/>
+          <div className="phoneNumber-title">Telefonnummer:</div>
+          <div className="phoneNumber-result">{traveller.PhoneNumber}</div>
+        </div>
+
+        <div className="price">
+          <br/>
+          <div className="price-title">Totalbelopp:</div>
+          <div className="price-result">{booking.Price} kr</div>
+        </div>
+      </div>
+
+      <div className="search-btn">
+        <button type="button" onClick={handleClick}>Avboka bokningen</button>
+      </div>
+    </>);
   }
 
-  // if we get error then it goes here, its not checking a specific thing at the moment.
+  // if we get error then it goes here, it's not checking a specific thing at the moment.
   function Error() {
-    return(
-      <>
-        <div>
-          Seems like something went wrong! (Error: {error})
-        </div>
-      </>
-    );
+    return (<>
+      <div>
+        Seems like something went wrong!<br/>
+        Error: {error}
+      </div>
+    </>);
   }
 
-  return (
-    <main>
-      { error ? <Error /> : <Booking /> }
-    </main>    
-  );
-};
+  return (<main>
+    {error ? <Error/> : <Booking/>}
+  </main>);
+}
 
 export default MyBookings2Page;
--- client/src/components/MyBookingsPage.jsx
+++ client/src/components/MyBookingsPage.jsx
@@ -1,43 +1,59 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import '../css/myBookingsPage.css';
 
 function MyBookingsPage() {
   let navigate = useNavigate();
+  const [idIsValid, setIdIsValid] = useState(true);
   const [formData, updateFormData] = useState({
     bookingId: ""
   });
 
   function handelChange(event) {
     updateFormData({
-      ...formData,
-      [event.target.name]: event.target.value.trim()
+      ...formData, [event.target.name]: event.target.value.trim()
     });
+    setIdIsValid(true);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    navigate('/MyBookings2Page', { state: formData });
+    (async () => {
+      if (await bookingIdIsValid(formData.bookingId)) {
+        navigate('/MyBookings2Page', {state: formData});
+      }
+    })();
+    setIdIsValid(false);
   }
 
-  return (
+  async function bookingIdIsValid(id) {
+    return (!isNaN(id) && await bookingExists(id));
+  }
+
+  async function bookingExists(id) {
+    return await fetch(`/api/booking/${id}`)
+      .then(response => {
+        return response.ok;
+      });
+  }
+
+    return (
     <main>
-      <div>
         <div>
-          <h1 className="page-title">Mina bokningar</h1>
+          <div>
+            <h1 className="page-title">Min bokning</h1>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="my-bookings-search">
+              <input className="search-bar" placeholder="Bokningsnummer" min="0" name="bookingId" value={formData.bookingId}
+                     onChange={handelChange}/> {idIsValid ? '' : '(Invalid Booking-ID)'}
+            </div>
+            <div className="search-btn">
+              <input type="submit" value="Sök bokning"/>
+            </div>
+          </form>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div className="my-bookings-search">
-            {/* <input type="number" min="0"  name="bookingId" class="search-bar" placeholder="Bokningsnummer"></input> */}
-            <input placeholder="Bokningsnummer" min="0" name="bookingId" value={formData.bookingId} onChange={handelChange}/>
-          </div>
-          <div className="search-btn">
-            {/* <button type="submit" value="Submit" onClick={handleClick}>Sök</button> */}
-            <input type="submit" value="My Bookings"/>
-          </div>
-        </form>    
-      </div>
-    </main>
-  );
+  </main>);
 };
 
 export default MyBookingsPage;
