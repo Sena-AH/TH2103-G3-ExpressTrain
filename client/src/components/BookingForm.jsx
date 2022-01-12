4efab0c011f72ded5111898b6f859851baadfe57
Joakim Widell <70150296+joakimwidell@users.noreply.github.com>
2022-01-12 09:38:46 +0100

    add dot to CSS - bookingform

--- client/src/components/BookingForm.jsx
+++ client/src/components/BookingForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { Context } from '../App';
-import './css/bookingForm.css';
+import '../css/bookingForm.css';
 
 
 function BookingForm(props) {
